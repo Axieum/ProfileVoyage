06b008a660c86997bbccc2e25b9e4f2e793cc2e0
Axieum <axieum@users.noreply.github.com>
2017-10-24 20:41:47 +1000

    General account settings submit button disabled until form is modified.

--- resources/views/account/general.blade.php
+++ resources/views/account/general.blade.php
@@ -41,7 +41,7 @@
                     <div class="field">
                         <label class="label">Name</label>
                         <div class="control has-icons-left has-icons-right">
-                            <input class="input" type="text" placeholder="{{ Auth::user()->profile->name }}" :class="{'is-danger': errors.has('name'), 'is-success': fields.name &amp;&amp; fields.name.valid }" v-validate="{rules:{alpha_spaces: true, max: 64, min: 2 }}" value="{{ old('name') }}" name="name">
+                            <input v-model="name" class="input" type="text" placeholder="e.g. John Doe" :class="{'is-danger': errors.has('name'), 'is-success': fields.name &amp;&amp; fields.name.valid }" v-validate="{rules:{alpha_spaces: true, max: 64, min: 2 }}" value="{{ old('name') }}" name="name">
                             <span class="icon is-small"><i class="fa fa-user-o"></i></span>
                         </div>
                         <p class="help is-danger" :show="errors.has('name')">@{{ errors.first('name') }}</p>
@@ -51,7 +51,7 @@
                     <div class="field">
                         <label class="label">Location</label>
                         <div class="control has-icons-left has-icons-right">
-                            <input class="input" type="text" placeholder="{{ Auth::user()->profile->location ?: 'e.g. Antarctica' }}" :class="{'is-danger': errors.has('location'), 'is-success': fields.location &amp;&amp; fields.location.valid }" v-validate="{rules:{alpha_spaces: true, max: 85, min: 1 }}" value="{{ old('location') }}" name="location">
+                            <input v-model="location" class="input" type="text" placeholder="e.g. Antarctica" :class="{'is-danger': errors.has('location'), 'is-success': fields.location &amp;&amp; fields.location.valid }" v-validate="{rules:{alpha_spaces: true, max: 85, min: 1 }}" value="{{ old('location') }}" name="location">
                             <span class="icon is-small"><i class="fa fa-globe"></i></span>
                         </div>
                         <p class="help is-danger" :show="errors.has('location')">@{{ errors.first('location') }}</p>
@@ -63,7 +63,7 @@
                         <div class="field has-addons">
                             <p class="control">
                                 <span class="select">
-                                    <select name="dob_day">
+                                    <select name="dob_day" v-model="dob_day">
                                         <option selected disabled>Day</option>
                                         @for ($i=1; $i <= 31; $i++)
                                             <option value="{{ $i }}" {{ date('d', strtotime(Auth::user()->profile->date_of_birth)) == $i ? 'selected' : '' }}>{{ $i }}</option>
@@ -73,7 +73,7 @@
                             </p>
                             <p class="control">
                                 <span class="select">
-                                    <select name="dob_month">
+                                    <select name="dob_month" v-model="dob_month">
                                         <option selected disabled>Month</option>
                                         @foreach (array(1 => 'January', 2 => 'February', 3 => 'March', 4 => 'April', 5 => 'May', 6 => 'June', 7 => 'July', 8 => 'August', 9 => 'September', 10 => 'October', 11 => 'November', 12 => 'December') as $month)
                                             <option value="{{ $loop->iteration }}" {{ date('m', strtotime(Auth::user()->profile->date_of_birth)) == $loop->iteration ? 'selected' : '' }}>{{ $month }}</option>
@@ -83,7 +83,7 @@
                             </p>
                             <p class="control">
                                 <span class="select">
-                                    <select name="dob_year">
+                                    <select name="dob_year" v-model="dob_year">
                                         <option selected disabled>Year</option>
                                         @for ($i=date('Y') - 13; $i >= date('Y') - 128; $i--)
                                             <option value="{{ $i }}" {{ date('Y', strtotime(Auth::user()->profile->date_of_birth)) == $i ? 'selected' : '' }}>{{ $i }}</option>
@@ -96,7 +96,7 @@
 
                     <div class="field columns m-t-15">
                         <div class="column is-4 is-offset-4">
-                            <button type="submit" class="button is-primary is-fullwidth">Save Changes!</button>
+                            <button type="submit" class="button is-primary is-fullwidth" :disabled="submittable == 0">Save Changes!</button>
                         </div>
                     </div>
                 </form>
@@ -108,7 +108,42 @@
 @section('scripts')
     <script>
         var app = new Vue({
-            el: 'main'
+            el: 'main',
+            data: {
+                oldName: "{{ Auth::user()->profile->name }}",
+                oldLocation: "{{ Auth::user()->profile->location }}",
+                oldDD: {{ date('d', strtotime(Auth::user()->profile->date_of_birth)) }},
+                oldDM: {{ date('m', strtotime(Auth::user()->profile->date_of_birth)) }},
+                oldDY: {{ date('Y', strtotime(Auth::user()->profile->date_of_birth)) }},
+                name: "{{ Auth::user()->profile->name }}",
+                location: "{{ Auth::user()->profile->location }}",
+                dob_day: {{ date('d', strtotime(Auth::user()->profile->date_of_birth)) }},
+                dob_month: {{ date('m', strtotime(Auth::user()->profile->date_of_birth)) }},
+                dob_year: {{ date('Y', strtotime(Auth::user()->profile->date_of_birth)) }},
+                submittable: false
+            },
+            watch: {
+                name: function() {
+                    this.openSesame();
+                },
+                location: function() {
+                    this.openSesame();
+                },
+                dob_day: function() {
+                    this.openSesame();
+                },
+                dob_month: function() {
+                    this.openSesame();
+                },
+                dob_year: function() {
+                    this.openSesame();
+                }
+            },
+            methods: {
+                openSesame: function() {
+                    this.submittable = this.oldName != this.name || this.oldLocation != this.location  || this.oldDD != this.dob_day || this.oldDM != this.dob_month || this.oldDY != this.dob_year;
+                }
+            }
         });
     </script>
 @endsection
