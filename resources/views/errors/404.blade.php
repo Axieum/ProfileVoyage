a5c932f8ae3325ccba60a7ef6e588daac14c7c33
Axieum <axieum@users.noreply.github.com>
2017-11-27 16:54:25 +1000

    Enhanced the 'Error 404' view to match the theme.

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,57 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.auth')
 
-        <title>Error 404</title>
+@section('title', 'Error 404')
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 36px;
-                padding: 20px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            <div class="content">
-                <div class="title">
-                    {{ $exception->getMessage() ?: 'Whoops! Page not found!' }}
-                </div>
-            </div>
-        </div>
-    </body>
-</html>
+@section('content')
+    <div id="app" class="container has-text-centered">
+        <h2 class="title is-2 white-text">Error 404</h2>
+        <p class="subtitle is-4 has-text-weight-light white-text">{{ $exception->getMessage() ?: 'Whoops! Page not found!' }}</p>
+    </div>
+@endsection
